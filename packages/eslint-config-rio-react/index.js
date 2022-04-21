6c5eed0fe430444c48e50562d907a2b50bd7a62e
Léon Chabbey <leon.chabbey@gmail.com>
2022-04-21 17:44:00 +0200

    refactor: extends conflict solved by duplicating rules for now

--- packages/eslint-config-rio-react/index.js
+++ packages/eslint-config-rio-react/index.js
@@ -3,10 +3,115 @@ module.exports = {
     "airbnb",
     "airbnb-typescript",
     "plugin:react-hooks/recommended",
-    "@srgssr/eslint-config-rio",
     "plugin:prettier/recommended",
   ],
   rules: {
+    /**********************************************
+     * (eslint-config-rio) START
+     *********************************************/
+    /**********************************************
+     * (eslint-config-rio-js) START
+     *********************************************/
+    "arrow-body-style": ["error", "as-needed"],
+    "import/extensions": "off",
+    "import/namespace": "off",
+    "import/no-default-export": "warn",
+    "import/no-extraneous-dependencies": "off",
+    "import/order": [
+      "error",
+      {
+        groups: ["builtin", "external", "internal"],
+        pathGroups: [
+          {
+            pattern: "react",
+            group: "external",
+            position: "before",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["react"],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+    "import/prefer-default-export": "off",
+    "import/no-cycle": "off",
+    "max-len": ["error", { code: 140, comments: 200 }],
+    "no-use-before-define": "off",
+    quotes: ["error", "single", { avoidEscape: true }],
+    camelcase: "error",
+    "no-nested-ternary": "off",
+    radix: "off",
+    "no-param-reassign": [
+      "error",
+      { props: true, ignorePropertyModificationsForRegex: ["^draft"] },
+    ],
+    "consistent-return": "off",
+    "max-classes-per-file": "off",
+    "prefer-regex-literals": 0,
+    /**********************************************
+     * (eslint-config-rio-js) END
+     *********************************************/
+    "@typescript-eslint/ban-ts-comment": "warn",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        args: "none",
+        ignoreRestSiblings: false,
+        vars: "all",
+      },
+    ],
+    "@typescript-eslint/no-use-before-define": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "arrow-body-style": ["error", "as-needed"],
+    "import/extensions": "off",
+    "import/namespace": "off",
+    "import/no-default-export": "warn",
+    "import/no-extraneous-dependencies": "off",
+    "import/order": [
+      "error",
+      {
+        groups: ["builtin", "external", "internal"],
+        pathGroups: [
+          {
+            pattern: "react",
+            group: "external",
+            position: "before",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["react"],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+    "import/prefer-default-export": "off",
+    "import/no-cycle": "off",
+    "max-len": ["error", { code: 140, comments: 200 }],
+    "no-use-before-define": "off",
+    quotes: ["error", "single", { avoidEscape: true }],
+    camelcase: "error",
+    "no-nested-ternary": "off",
+    radix: "off",
+    "no-param-reassign": [
+      "error",
+      { props: true, ignorePropertyModificationsForRegex: ["^draft"] },
+    ],
+    "consistent-return": "off",
+    "max-classes-per-file": "off",
+    "@typescript-eslint/no-unused-expressions": [
+      "error",
+      { allowShortCircuit: true, allowTernary: true },
+    ],
+    "prefer-regex-literals": 0,
+    /**********************************************
+     * (eslint-config-rio) END
+     *********************************************/
     "react/jsx-props-no-spreading": "off",
     "react/display-name": "off",
     "react/jsx-filename-extension": [
--- packages/eslint-config-rio/index.js
+++ packages/eslint-config-rio/index.js
@@ -1,12 +1,57 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
   extends: [
+    "airbnb/base",
     "airbnb-typescript/base",
     "plugin:@typescript-eslint/recommended",
-    "@srgssr/eslint-config-rio-js",
     "plugin:prettier/recommended",
   ],
   rules: {
+    /**********************************************
+     * (eslint-config-rio-js) START
+     *********************************************/
+     "arrow-body-style": ["error", "as-needed"],
+     "import/extensions": "off",
+     "import/namespace": "off",
+     "import/no-default-export": "warn",
+     "import/no-extraneous-dependencies": "off",
+     "import/order": [
+       "error",
+       {
+         groups: ["builtin", "external", "internal"],
+         pathGroups: [
+           {
+             pattern: "react",
+             group: "external",
+             position: "before",
+           },
+         ],
+         pathGroupsExcludedImportTypes: ["react"],
+         "newlines-between": "always",
+         alphabetize: {
+           order: "asc",
+           caseInsensitive: true,
+         },
+       },
+     ],
+     "import/prefer-default-export": "off",
+     "import/no-cycle": "off",
+     "max-len": ["error", { code: 140, comments: 200 }],
+     "no-use-before-define": "off",
+     quotes: ["error", "single", { avoidEscape: true }],
+     camelcase: "error",
+     "no-nested-ternary": "off",
+     radix: "off",
+     "no-param-reassign": [
+       "error",
+       { props: true, ignorePropertyModificationsForRegex: ["^draft"] },
+     ],
+     "consistent-return": "off",
+     "max-classes-per-file": "off",
+     "prefer-regex-literals": 0,
+     /**********************************************
+     * (eslint-config-rio-js) END
+     *********************************************/
     "@typescript-eslint/ban-ts-comment": "warn",
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-unused-vars": [
